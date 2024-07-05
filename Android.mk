4a588bdd15ae573dfbf5ee80e4dfdb6f5a022b7a
fjrXTR <fajarslebew31@gmail.com>
2024-07-05 06:33:27 +0000

    X6833B: Move Audio blobs to the Device tree for each device * The previous list of blobs would not guarantee Audio would work on some transsion devices using the MT6789, so we moved them to their respective device trees
    Change-Id: I42ef2b08643d51afd890185367ab8f2ce04f79fa
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- Android.mk
+++ Android.mk
@@ -10,12 +10,23 @@ ifeq ($(TARGET_DEVICE),X6833B)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+AUDIO_SYMLINKS := \
+	$(TARGET_OUT_VENDOR)/lib/hw/audio.primary.$(TARGET_BOARD_PLATFORM).so \
+	$(TARGET_OUT_VENDOR)/lib/hw/audio.r_submix.$(TARGET_BOARD_PLATFORM).so \
+	$(TARGET_OUT_VENDOR)/lib64/hw/audio.primary.$(TARGET_BOARD_PLATFORM).so \
+	$(TARGET_OUT_VENDOR)/lib64/hw/audio.r_submix.$(TARGET_BOARD_PLATFORM).so
+
+$(AUDIO_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Linking $@"
+	@ln -sf $(subst $(TARGET_BOARD_PLATFORM),mediatek,$(notdir $@)) $@
+
 CAMERA_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/, $(strip $(shell cat $(DEVICE_PATH)/symlink/camera.txt)))
 $(CAMERA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) echo "Linking $(notdir $@)"
 	@ln -sf $(TARGET_BOARD_PLATFORM)/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += \
+	$(AUDIO_SYMLINKS) \
 	$(CAMERA_SYMLINKS)
 
 endif
